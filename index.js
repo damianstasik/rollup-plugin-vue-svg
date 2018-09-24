dac416e658d767fe6013e48183e99f409aacc2ff
Damian Stasik <visualfanatic@users.noreply.github.com>
2018-09-24 22:23:41 +0200

    Plugin is functional, but far from being stable

---
+++ index.js
@@ -0,0 +1,29 @@
+const svgToVue = require('svg-to-vue');
+const { createFilter } = require('rollup-pluginutils');
+
+export default function svg(options = {}) {
+  const { svgo, include, exclude } = options;
+  const filter = createFilter(include, exclude);
+
+  return {
+    name: 'vue-svg',
+    async transform(content, id) {
+      if (!id.endsWith('.svg') || !filter(id)) {
+        return null;
+      }
+
+      const code = await svgToVue(content, {
+        svgoConfig: svgo,
+        svgoPath: id,
+        esmExport: true,
+      });
+
+      return {
+        code,
+        map: {
+          mappings: '',
+        },
+      };
+    },
+  };
+}
