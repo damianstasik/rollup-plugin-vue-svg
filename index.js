a815ef5f0c1efe6a8f176a47d3138c597b5119cb
Damian Stasik <visualfanatic@users.noreply.github.com>
2018-10-11 22:32:55 +0200

    Update `svg-to-vue` dep

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ export default function svg(options = {}) {
       const code = await svgToVue(content, {
         svgoConfig: svgo,
         svgoPath: id,
-        esmExport: true,
       });
 
       return {
